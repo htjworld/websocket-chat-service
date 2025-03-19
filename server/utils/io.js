1ae5deff758cd8c3c5a3956ee3753aeda28ec177
htjworld <116538001+htjworld@users.noreply.github.com>
2025-03-19 19:56:53 +0900

    (feat) 웹소켓으로 전송된 메세지 전달

--- server/utils/io.js
+++ server/utils/io.js
@@ -1,4 +1,6 @@
+const chatController = require("../Controllers/chat.controller");
 const userController = require("../Controllers/user.controller");
+const chat = require("../Models/chat");
 
 module.exports = function (io) {
   //io~~ => emit:듣는것, on:말하기
@@ -10,6 +12,19 @@ module.exports = function (io) {
       try {
         const user = await userController.saveUser(userName, socket.id);
         cb({ ok: true, data: user });
+      } catch (error) {
+        cb({ ok: false, error: error.message });
+      }
+    });
+
+    socket.on("sendMessage", async (message, cb) => {
+      try {
+        // 유저 찾기(socket.id로)
+        const user = await userController.checkUser(socket.id);
+        // 메세지 저장
+        const newMessage = await chatController.saveChat(message, user);
+        io.emit("message", newMessage);
+        cb({ ok: true });
       } catch (error) {
         cb({ ok: false, error: error.message });
       }
