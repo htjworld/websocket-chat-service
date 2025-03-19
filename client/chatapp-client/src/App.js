2ec595e1f6a5672f830e0e841d7e79b1a53db92b
htjworld <116538001+htjworld@users.noreply.github.com>
2025-03-19 19:43:37 +0900

    (feat) 메세지 전송하기

--- client/chatapp-client/src/App.js
+++ client/chatapp-client/src/App.js
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import socket from "./server";
+import InputField from "./components/InputField/InputField";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     // 틀자마자 실행
@@ -14,14 +16,26 @@ function App() {
     console.log("uuu", userName);
 
     socket.emit("login", userName, (res) => {
-      if(res?.ok){
-        setUser(res.data)
+      if (res?.ok) {
+        setUser(res.data);
       }
+    });
+  };
+  const sendMessage = (event) => {
+    event.preventDefault();
+    socket.emit("sendMessage", message, (res) => {
+      console.log("sendMessage res", res);
     });
   };
   return (
     <div>
-      <div className="App"></div>
+      <div className="App">
+        <InputField
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
     </div>
   );
 }
