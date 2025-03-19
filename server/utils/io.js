0e7f96ac42f0872213c64b8dff5b9271893d84a6
htjworld <116538001+htjworld@users.noreply.github.com>
2025-03-19 19:13:22 +0900

    (feat) 유저 연결 끊김 로그 추가 및 포트 번호 바꿈(5001)

--- server/utils/io.js
+++ server/utils/io.js
@@ -2,6 +2,11 @@ module.exports = function(io){
     //io~~ => emit:듣는것, on:말하기
     io.on("connection", async(socket)=>{
         console.log("client is connected", socket.id);
-        
+
+
+        socket.on("disconnect", ()=>{
+            console.log("user is disconnected");
+            
+        })
     });
 };
