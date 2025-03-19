a4b58d049e5e925e22b972821929d6b46dfae958
htjworld <116538001+htjworld@users.noreply.github.com>
2025-03-19 19:31:28 +0900

    (feat) 유저 받아서 저장하는 로직 추가

--- server/utils/io.js
+++ server/utils/io.js
@@ -1,12 +1,22 @@
-module.exports = function(io){
-    //io~~ => emit:듣는것, on:말하기
-    io.on("connection", async(socket)=>{
-        console.log("client is connected", socket.id);
+const userController = require("../Controllers/user.controller");
 
+module.exports = function (io) {
+  //io~~ => emit:듣는것, on:말하기
+  io.on("connection", async (socket) => {
+    console.log("client is connected", socket.id);
 
-        socket.on("disconnect", ()=>{
-            console.log("user is disconnected");
-            
-        })
+    socket.on("login", async (userName, cb) => {
+      //유저 정보를 저장
+      try {
+        const user = await userController.saveUser(userName, socket.id);
+        cb({ ok: true, data: user });
+      } catch (error) {
+        cb({ ok: false, error: error.message });
+      }
     });
+
+    socket.on("disconnect", () => {
+      console.log("user is disconnected");
+    });
+  });
 };
