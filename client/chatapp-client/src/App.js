9da3e36e2b711c68ccb88317521427c099b5f955
htjworld <116538001+htjworld@users.noreply.github.com>
2025-03-19 19:18:25 +0900

    (feat) userName 받아서 웹소켓으로 서버에 보내기

--- client/chatapp-client/src/App.js
+++ client/chatapp-client/src/App.js
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
 import "./App.css";
 import socket from "./server";
 
 function App() {
+
+  const askUserName=()=>{
+    useEffect(()=>{ // 틀자마자 실행
+      askUserName()
+    },[])
+    const userName = prompt("당신의 이름을 입력하세요");
+    console.log("uuu",userName)
+
+    socket.emit("login",userName,(res)=>{
+        console.log("Res",res);
+        
+    })
+  }
   return (
     <div>
       <div className="App"></div>
