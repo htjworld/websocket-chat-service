5919ecab000774391fa1adcb20e57edcf8345c46
htjworld <116538001+htjworld@users.noreply.github.com>
2025-03-27 17:48:21 +0900

    (refactor) 방 생성 코드 로직 수정

--- server/app.js
+++ server/app.js
@@ -15,22 +15,21 @@ mongoose
 
 //임의의 room
 app.get("/", async (req, res) => {
-  Room.insertMany([
-    {
-      room: "자바스크립트 단톡방",
-      members: [],
-    },
-    {
-      room: "리액트 단톡방",
-      members: [],
-    },
-    {
-      room: "NodeJS 단톡방",
-      members: [],
-    },
-  ])
-    .then(() => res.send("ok"))
-    .catch((error) => res.send(error));
+  try {
+    const rooms = await Room.find({});
+    if (rooms.length === 0) {
+      await Room.insertMany([
+        { room: "자바스크립트 단톡방", members: [] },
+        { room: "리액트 단톡방", members: [] },
+        { room: "NodeJS 단톡방", members: [] },
+      ]);
+      return res.send("room created");
+    } else {
+      return res.send("rooms already exist");
+    }
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 });
 
 module.exports = app;
