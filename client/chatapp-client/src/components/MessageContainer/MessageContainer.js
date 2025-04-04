130d91696348e1dbdd78e8e8280a68460b88bd13
htjworld <116538001+htjworld@users.noreply.github.com>
2025-04-04 16:14:14 +0900

    (style) timestamp ui 수정

--- client/chatapp-client/src/components/MessageContainer/MessageContainer.js
+++ client/chatapp-client/src/components/MessageContainer/MessageContainer.js
@@ -27,29 +27,25 @@ const MessageContainer = ({ messageList, user }) => {
               </div>
             ) : isMyMessage ? (
               <div className="my-message-container">
-                <div className="my-message">{message.chat}</div>
                 <span className="timestamp">{time}</span>
+                <div className="my-message">{message.chat}</div>
               </div>
             ) : (
               <div className="your-message-container">
                 <img
                   src="/profile.jpeg"
                   className="profile-image"
-                  style={
-                    (index === 0
-                      ? { visibility: "visible" }
-                      : messageList[index - 1].user.name === user.name) ||
-                    messageList[index - 1].user.name === "system"
-                      ? { visibility: "visible" }
-                      : { visibility: "hidden" }
-                  }
+                  style={{ visibility: isDifferentUser ? "visible" : "hidden" }}
+                
                 />
                 <div className="your-message-group">
                 {isDifferentUser && (
                   <div className="message-author">{message.user.name}</div>
                 )}
+                <div className="message-set">
                 <div className="your-message">{message.chat}</div>
                 <span className="timestamp">{time}</span>
+                </div>
                 </div>
               </div>
             )}
